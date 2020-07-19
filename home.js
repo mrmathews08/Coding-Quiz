f999a0767f52d7f933981758b5e873bca32d3057
Tyler Mathews <mrmathews08@gmail.com>
2020-07-19 13:30:55 -0600

    timer

--- home.js
+++ home.js
@@ -40,19 +40,40 @@ var questions = [
 
 var score = 0;
 var questionIndex = 0;
+
 var wrapper = document.querySelector("#wrapper");
-var displayTime = document.querySelector("#time");
+var displayTime = document.querySelector("#currentTime");
 var timer = document.querySelector("#startQuiz");
-var questionsBox = document.querySelector("#questionsDiv");
+var questionsDiv = document.querySelector("#questionsDiv");
 
 // These variables are going to be associated with my timer
 
+var timeLeft = 76;
 
+var holdInterval = 0;
+
+var penalty = 10;
+
+var createUl = document.querySelector("ul");
 
 //Timer button
 
 timer.addEventListener("click", function () {
-    
+
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            timeLeft--;
+            displayTime.textContent = "Time: " + timeLeft;
+
+            if (timeLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                displayTime.textContent = "Time is up!";
+            }
+
+        }, 1000);
+    }
+    render(questionIndex);
 });
 
 
