b87d54684907d60b92089452e025607b908a6506
Tyler Mathews <mrmathews08@gmail.com>
2020-07-19 13:06:31 -0600

    home.js start

---
+++ home.js
@@ -0,0 +1,58 @@
+// first variable is a doozy, it will be an ARRAY that contains my questions.
+
+var questions = [
+    {
+        question: "",
+        options: "",
+        answer: ""
+
+    },
+    {
+        question: "",
+        options: "",
+        answer: ""
+
+    },
+    {
+        question: "",
+        options: "",
+        answer: ""
+
+    },
+    {
+        question: "",
+        options: "",
+        answer: ""
+
+    },
+    {
+        question: "",
+        options: "",
+        answer: ""
+
+    },
+
+
+];
+
+// Declare some variables here, use the querySelector to target correct elements.
+
+
+var score = 0;
+var questionIndex = 0;
+var wrapper = document.querySelector("#wrapper");
+var displayTime = document.querySelector("#time");
+var timer = document.querySelector("#startQuiz");
+var questionsBox = document.querySelector("#questionsDiv");
+
+// These variables are going to be associated with my timer
+
+
+
+//Timer button
+
+timer.addEventListener("click", function () {
+    
+});
+
+
