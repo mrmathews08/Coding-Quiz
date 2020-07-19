ff2b66cb082d465afd61883505c1150765e3e508
Tyler Mathews <mrmathews08@gmail.com>
2020-07-19 14:05:14 -0600

    alldonefunction

--- home.js
+++ home.js
@@ -2,9 +2,9 @@
 
 var questions = [
     {
-        question: "",
-        options: "",
-        answer: ""
+        question: "What is the HTML tag under which one can write the JavaScript code?",
+        options: ["A) <javascript>", "B) <scripted>", "C) <script>", "D) <js>"],
+        answer: "<script>"
 
     },
     {
@@ -60,6 +60,8 @@ var createUl = document.querySelector("ul");
 
 timer.addEventListener("click", function () {
 
+//Set to 0, checking for 0
+
     if (holdInterval === 0) {
         holdInterval = setInterval(function () {
             timeLeft--;
@@ -76,4 +78,61 @@ timer.addEventListener("click", function () {
     render(questionIndex);
 });
 
+// Need to render the questions to come up.
 
+function render(questionIndex) {
+    //Clear it first, empty string
+
+    questionsDiv.innerHTML = "";
+    createUl.innerHtml = "";
+
+    //For loop here to search array for information
+
+    for (var i = 0; i < questions.length; i++) {
+        //order of information displayed
+        var userQuestion = questions[questionIndex].question;
+        var userChoices = questions[questionIndex].options;
+        questionsDiv.textContent = userQuestion;
+    }
+    
+    //For each loop new for each option
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        questionsDiv.appendChild(createUl);
+        createUl.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
+
+//compare click event
+
+function compare(event) {
+    var element = event.target;
+
+    if (element.matches("li")) {
+        
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "creatDiv");
+        //conditional for correct
+        if (element.textContent == questions[questionIndex].answer) {
+            score++;
+            createDiv.textContent = "Correct!";
+        } else {
+            timeLeft = timeLeft - penalty;
+            createDiv.textContent = "wrong, the correct answer is: " + questions[questionsIndex].answer;
+        }
+    }
+    //adds one to the number question that the user is on
+    questionIndex++;
+
+    if (questionIndex >= questions.length) {
+
+        allDone();
+        createDiv.textContent = "THE END!!" + "your score is " + score + "/" + questions.length + " Correct!";
+    } else {
+        render(questionIndex);
+    }
+    questionsDiv.appendChild(createDiv);
+
+}
